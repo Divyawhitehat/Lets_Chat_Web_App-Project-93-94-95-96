193e40b4391ccda236b7abc4565b81bc37428119
Divyawhitehat <96724286+Divyawhitehat@users.noreply.github.com>
2023-03-25 12:11:35 +0530

    Add files via upload

---
+++ Lets_Chat_.js
@@ -0,0 +1,10 @@
+function adduser() 
+{
+    user_name = document.getElementById("user_name").value;
+    localStorage.setItem("User Name",user_name);
+    window.location = "kwitter_room.html";
+}
+function exituser()
+{
+    
+}
---
+++ Lets_Chat_room.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBVWLcLRw3w-CNDzVerVfIzc7zQ4OYD8A4",
+    authDomain: "kwitter-re.firebaseapp.com",
+    databaseURL: "https://kwitter-re-default-rtdb.firebaseio.com",
+    projectId: "kwitter-re",
+    storageBucket: "kwitter-re.appspot.com",
+    messagingSenderId: "365700024414",
+    appId: "1:365700024414:web:f1824da9c14c5fccb80ccf"
+  };
